d5fa6b22d9bfcdc0c4676f8b28fa8f8c810f19af
owalid <othmane.el.ayadi@gmail.com>
2020-06-13 02:29:00 -0400

    parser ok

--- parser.js
+++ parser.js
@@ -33,12 +33,19 @@ const pow = (number, pow) => {
 const parser = (expression) => {
   expression = expression.trim();
   expression = expression.split(' ').join('');
+  expression = expression.toUpperCase()
   if (expression.includes('=') && expression.split('=')[1] !== 0) {
     console.log("reduceExpression");
   }
 
-  const split_degree = expression.split('^')
-  const degree_number = +split_degree[split_degree.length - 1].charAt(0);
+  let degree_number;
+  if (expression.includes('^')) {
+    const split_degree = expression.split('^')
+    degree_number = +split_degree[split_degree.length - 1].charAt(0);
+  } else {
+    const split_degree = expression.split('X')
+    degree_number = +split_degree[split_degree.length - 1].charAt(0);
+  }
   let result =  {degree_number: degree_number}
   if (degree_number === 2) {
     result.solutions = degree_2(expression);
@@ -51,8 +58,9 @@ const parser = (expression) => {
 }
 
 const degree_1 = (expression) => {
-  const a = +expression.split("X^0")[1].split("X^1")[0].split('*').join('') || 0;
-  const b = +expression.split("X^0")[0].split('*').join('') || 1 || 0;
+  const have_pow_char = expression.includes('^');
+  const a = +expression.split(`${have_pow_char ? 'X^0' : 'X0'}`)[1].split(`${have_pow_char ? 'X^1' : 'X1'}`)[0].split('*').join('') || 0;
+  const b = +expression.split(`${have_pow_char ? 'X^0' : 'X0'}`)[0].split('*').join('') || 1 || 0;
   let result = {};
 
   if (a > 0) {
@@ -62,9 +70,11 @@ const degree_1 = (expression) => {
 }
 
 const degree_2 = (expression) => {
-  const a = +expression.split("X^0")[1].split("X^1")[1].split("X^2")[0].split('*').join('') || 1;
-  const b = +expression.split("X^0")[1].split("X^1")[0].split('*').join('') || 1;
-  const c = +expression.split("X^0")[0].split('*').join('') || 1;
+  const have_pow_char = expression.includes('^');
+  const a = +expression.split(`${have_pow_char ? 'X^0' : 'X0'}`)[1].split(`${have_pow_char ? 'X^1' : 'X1'}`)[1].split(`${have_pow_char ? 'X^2' : 'X2'}`)[0].split('*').join('') || 1;
+  const b = +expression.split(`${have_pow_char ? 'X^0' : 'X0'}`)[1].split(`${have_pow_char ? 'X^1' : 'X1'}`)[0].split('*').join('') || 1;
+  const c = +expression.split(`${have_pow_char ? 'X^0' : 'X0'}`)[0].split('*').join('') || 1;
+  console.log(a, b, c)
   let delta = pow(b, 2) - (4 * (a * c));
   let result = {};
 
@@ -83,4 +93,4 @@ const degree_2 = (expression) => {
   return (result);
 }
 
-parser("111 * X^0 + 14 * X^1 + 1 * X^2")
+parser("111 X0 + 14 X1 + 1 X2")
