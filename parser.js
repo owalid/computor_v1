13b3fc20fa7b134d29dd84665458ef391947437f
owalid <othmane.el.ayadi@gmail.com>
2020-06-13 02:16:23 -0400

    clean code

--- parser.js
+++ parser.js
@@ -1,13 +1,16 @@
-const separator = ["+", "*", "/", "-"];
+const operator = ["+", "*", "/", "-"];
 
 const sqrt = (number) => {
-  var lo = 0, hi = number;
-    while(lo <= hi) {
-      var mid = Math.floor((lo + hi) / 2);
-      if (mid * mid > number) hi = mid - 1;
-      else lo = mid + 1;
-    }
-    return hi;
+  let lo = 0, hi = number;
+  while(lo <= hi) {
+    let mid = Math.floor((lo + hi) / 2);
+    if (mid * mid > number) {
+      hi = mid - 1;
+    } else {
+      lo = mid + 1;
+    } 
+  }
+  return hi;
 }
 
 let gcd = (num, denom) => {
@@ -36,24 +39,26 @@ const parser = (expression) => {
 
   const split_degree = expression.split('^')
   const degree_number = +split_degree[split_degree.length - 1].charAt(0);
-
+  let result =  {degree_number: degree_number}
   if (degree_number === 2) {
-    degree_2(expression);
+    result.solutions = degree_2(expression);
   } else if (degree_number === 1) {
-    degree_1(expression);
+    result.solutions = degree_1(expression);
   } else {
     console.log("sorry")
   }
+  console.log(result)
 }
 
 const degree_1 = (expression) => {
   const a = +expression.split("X^0")[1].split("X^1")[0].split('*').join('') || 0;
   const b = +expression.split("X^0")[0].split('*').join('') || 1 || 0;
-  let result = {}
+  let result = {};
 
   if (a > 0) {
     result.x = b / a;
   }
+  return (result);
 }
 
 const degree_2 = (expression) => {
@@ -64,18 +69,18 @@ const degree_2 = (expression) => {
   let result = {};
 
   if (delta < 0) {
-    const reduce = reducer(b, (2* a))
+    const reduce = reducer(b, (2* a));
     let z1 = `${-reduce.numerator} - ${Math.sqrt(-delta)}i${(reduce.denominator ===  1) ? '' : ' / ' + reduce.denominator }`;
     let z2 = `${-reduce.numerator} + ${Math.sqrt(-delta)}i${(reduce.denominator === 1) ? '' : ' / ' + reduce.denominator }`;
     result.z1 = z1.toString();
     result.z2 = z2.toString();
   } else if (delta > 0) {
-    result.x1 = parseFloat((-b - Math.sqrt(delta)) / (2 * a), 3)
-    result.x2 = parseFloat((-b + Math.sqrt(delta)) / (2 * a), 3)
+    result.x1 = parseFloat((-b - Math.sqrt(delta)) / (2 * a), 3);
+    result.x2 = parseFloat((-b + Math.sqrt(delta)) / (2 * a), 3);
   } else {
-    result.x0 = parseFloat(-b / (2 * a))
+    result.x0 = parseFloat(-b / (2 * a));
   }
-  console.log(result)
+  return (result);
 }
 
 parser("111 * X^0 + 14 * X^1 + 1 * X^2")
